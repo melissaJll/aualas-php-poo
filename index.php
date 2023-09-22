4eff4f85febe830eecb3ae943531a00cd8e95f7b
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-09-22 09:47:27 -0300

    método setter

--- index.php
+++ index.php
@@ -3,23 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 2 </title>
+    <title>Exemplo 4 </title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 2</h1>
+    <h1>PHP com POO - Exemplo 4</h1>
     <hr>
 
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Método construtor com atribuição obrigatória de parametros/propriedades do objeto</li>
-        <li>Uso de <code>$this</code> para acesso às propriedades dentro do objeto</li>
+        <li>Encapsulamento</li>
+        <li>Modificadores de visibilidade(public, private, protected)</li>
     </ul>
 
 <?php require_once "src/Cliente.php";
-$ClienteA = new Cliente("Ana", "anaB@email.com");
-$ClienteB = new Cliente("Ozzy", "ozzyy@email.com");
+$ClienteA = new Cliente;
+//Usando setter para definir nome
+$ClienteA->setNome("Ana");
 ?>
+<pre><?=var_dump($ClienteA)?></pre>
 
-<pre><?=var_dump($ClienteA, $ClienteB )?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,19 +1,13 @@
 <?php
 class Cliente{
-    public string $nome;
-    public string $email ="";
-    public string $senha;
-    public array $telefones;
+    private string $nome;
+    private string $email;
+    private string $senha; 
 
-    // Forma de inicialização de objetos, exigindo a tribuição de dados no momento de criar o objeto/instancia. O construtor é de sempre chamado de forma automática assim que o objeto é criado
-
-    public function __construct(string $name, string $enderecoEmail) {
-        //atribuindo aos atributos os valores que serão inseridos nos parametros
-        //this para acessar os atributos da classe
-        $this->nome = $name;
-        $this->email =  $enderecoEmail;
+    //getters/  setters
+    public function setNome(string $nome):void{
+        $this->nome = $nome;
     }
-
 
 }
 
