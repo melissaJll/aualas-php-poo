2bd32c52e5aaf0b2869a15bd7d0cdfb6ad4af2f9
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-09-29 11:30:07 -0300

    load

--- index.php
+++ index.php
@@ -21,14 +21,9 @@ use Fornecedor\Pagamento;
 use Prestador\Pagamento as PrestadorPagamento;
 use Tabajara\{MEI , PessoaFisica as PF , PessoaJuridica as PJ};
 
-require_once "src/fornecedores/Pagamento.php";
-require_once "src/prestadores/Pagamento.php";
+require_once "vendor/autoload.php";
 
-require_once "src/PessoaFisica.php";
-require_once "src/PessoaJuridica.php";
-require_once "src/MEI.php";
-
- $pagamentoFornecedor = new Pagamento;
+$pagamentoFornecedor = new Pagamento;
 $pagamentoPrestador = new PrestadorPagamento;
 
 $clientePF = new PF;
@@ -48,8 +43,6 @@ $clienteMEI->setNome("Maria");
 $clienteMEI->setAreaAtuacao("Artesanato");
 ?>
 
-<pre><?=var_dump($pagamentoFornecedor)?></pre>
-<pre><?=var_dump($pagamentoPrestador)?></pre>
 
 <h3>Cliente Pessoa Fisica</h3>
 <ul>
--- src/MEI.php
+++ src/MEI.php
@@ -1,6 +1,6 @@
 <?php
 namespace Tabajara;
-require_once "PessoaJuridica.php";
+
 final class MEI extends PessoaJuridica{
 // 'final' é a última classe de sua geração, não permite criar sub-classes a partir desta
     private string $areaAtuacao;
